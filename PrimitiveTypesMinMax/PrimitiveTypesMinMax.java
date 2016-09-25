9c7e9bec6a836164b6a00de75d48fc0a8ab67593
mnandan06 <mnandan06@gmail.com>
2016-09-25 23:49:17 +0530

    PrimitiveTypesMinMax

---
+++ PrimitiveTypesMinMax/PrimitiveTypesMinMax.java
@@ -0,0 +1,36 @@
+public class PrimitiveTypesMinMax
+{
+  public static void main(String[]args)
+  {
+    //int (32-bit signed integer)
+    System.out.println("int(min)="+Integer.MIN_VALUE);
+    System.out.println("int(max)="+Integer.MAX_VALUE);
+    System.out.println("int(bit-length)="+Integer.SIZE+"\n");
+
+    // byte (8-bit signed integer)
+    System.out.println("byte(min)="+Byte.MIN_VALUE);
+    System.out.println("byte(max)="+Byte.MAX_VALUE);
+    System.out.println("byte(bit-length)="+Byte.SIZE+"\n");
+    // short (16-bit signed integer)
+    System.out.println("short(min)="+Short.MIN_VALUE);
+    System.out.println("short(max)="+Short.MAX_VALUE);
+    System.out.println("short(bit-length)="+Short.SIZE+"\n");
+    // long (64-bit signed integer)
+    System.out.println("long(min)="+Long.MIN_VALUE);
+    System.out.println("long(max)="+Long.MAX_VALUE);
+    System.out.println("long(bit-length)="+Long.SIZE+"\n");
+    // char (16-bit signed integer)
+    System.out.println("char(min)="+Character.MIN_VALUE);
+    System.out.println("char(max)="+Character.MAX_VALUE);
+    System.out.println("char(bit-length)="+Character.SIZE+"\n");
+    // float (32-bit signed integer)
+    System.out.println("float(min)="+Float.MIN_VALUE);
+    System.out.println("float(max)="+Float.MAX_VALUE);
+    System.out.println("float(bit-length)="+Float.SIZE+"\n");
+    // double (64-bit signed integer)
+    System.out.println("double(min)="+Double.MIN_VALUE);
+    System.out.println("double(max)="+Double.MAX_VALUE);
+    System.out.println("double(bit-length)="+Double.SIZE+"\n");
+
+  }
+}
