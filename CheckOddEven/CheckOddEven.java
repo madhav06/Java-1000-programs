b034590c3126c50acf26bbfb3a2871f932c8e3cf
mnandan06 <mnandan06@gmail.com>
2016-09-27 11:37:58 +0530

    Check and confirm either number be even or odd?

---
+++ CheckOddEven/CheckOddEven.java
@@ -0,0 +1,21 @@
+
+
+public class CheckOddEven
+{
+  public static void main(String[] args)
+  {
+    int number = 34;
+    System.out.println("The number is: "+number);
+
+    if(number%2 == 1)
+    {
+      System.out.println("ODD");
+    }
+    else
+    {
+      System.out.println("EVEN");
+    }
+      System.out.println("BYE!");
+
+  }
+}
