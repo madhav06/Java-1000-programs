36581b93a4ea93dfe55159e6ee4da98e9b3287d0
mnandan06 <mnandan06@gmail.com>
2016-09-27 11:50:24 +0530

    check pass or fail

---
+++ CheckPassFail/CheckPassFail.java
@@ -0,0 +1,21 @@
+
+
+public class CheckPassFail
+{
+  public static void main(String[] args)
+  {
+    int mark = 49;
+    System.out.println("The marks is: "+mark);
+
+    if(mark >= 50)
+    {
+      System.out.println("Pass");
+    }
+    else
+    {
+      System.out.println("Fail");
+    }
+      System.out.println("Done");
+
+  }
+}
