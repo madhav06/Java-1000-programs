5743f0c4daad7d2e7a427fbdf2efd56138a66e7b
mnandan06 <mnandan06@gmail.com>
2016-10-07 14:23:24 +0530

    program Animal Inheritance in java

---
+++ Animal_Inheritance/MainClass.java
@@ -0,0 +1,22 @@
+
+public class MainClass {
+
+  public static void main(String[] args) {
+
+    Animal a = new Animal();
+    Bird b = new Bird();
+    Dog d = new Dog();
+
+    System.out.println( );
+
+    a.sleep();
+    a.eat();
+
+    b.sleep();
+    b.eat();
+
+    d.sleep();
+    d.eat();
+    
+  }
+}
