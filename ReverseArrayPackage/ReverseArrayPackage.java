8d0d69e9ded79cba8a8d8e2f418e2aab5a7600eb
mnandan06 <mnandan06@gmail.com>
2016-11-24 09:14:45 +0530

    ReverseArrayPackage, Palindrome

---
+++ ReverseArrayPackage/ReverseArrayPackage.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+
+public class ReverseArrayPackage {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i;
+        int arr[] = new int[n];
+        for( i=0; i < n; i++){
+            arr[i] = in.nextInt();
+          //  System.out.println(""+arr[i]);
+
+
+              //System.out.print(" "+arr[arr_i]);
+            //int size = arr.length;
+
+
+        }
+        for(i = 0; i < n ; i++){
+        System.out.print(" "+arr[(n-1) - i]);
+}
+}
+
+  }
